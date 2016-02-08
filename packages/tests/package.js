194acb3d39c4f8e98800887d80460ff997f64152
Loren Sands-Ramshaw <lorensr@gmail.com>
2016-02-08 00:12:21 -0500

    adding @1.0.0

--- packages/tests/package.js
+++ packages/tests/package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'tests',
-  version: '0.0.1',
+  version: '0.0.1'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use('accounts-base');
   api.use('underscore');
-  api.use('dburles:collection-helpers');
+  api.use('dburles:collection-helpers@1.0.0');
   api.addFiles('tests.js');
 });
 
